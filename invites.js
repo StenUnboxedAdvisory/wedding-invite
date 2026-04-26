7d08ceb5f7edf2eb24969a33e031d80eb49a3857
StenUnboxedAdvisory <sten@unboxedadvisory.nl>
2026-04-26 10:12:12 +0200

    Create invites.js

---
+++ invites.js
@@ -0,0 +1,9 @@
+// Token -> uitnodigingsinfo
+// type: "day" of "evening"
+// maxPeople: max. aantal personen op deze uitnodiging
+// label: naam/aanhef (voor bovenaan de pagina)
+
+window.INVITES = {
+  "AB12CD34EF": { type: "day",     maxPeople: 2, label: "Familie Jansen" },
+  "K9P2M4Q7R1": { type: "evening", maxPeople: 1, label: "Sanne" }
+};
