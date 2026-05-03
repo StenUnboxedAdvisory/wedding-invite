ee25a19bc6c67ae2659d930912577f48f04d2f03
StenUnboxedAdvisory <sten@unboxedadvisory.nl>
2026-05-03 20:28:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -195,10 +195,10 @@ window.addEventListener("DOMContentLoaded", () => {
   function showInvite(token, invite) {
     // show protected content
     if (gate) gate.classList.add("hidden");
-
+    
     if (protectedWrap) {
-      protectedWrap.classList.remove("hidden");   // ✅ haal hidden weg
-      protectedWrap.classList.add("is-visible");  // ✅ toon content
+      protectedWrap.classList.remove("hidden");
+      protectedWrap.classList.add("is-visible");
     }
 
     // store token
